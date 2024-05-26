dff20c0687fbf1d21aadbefd79f4260744adf802
Narayan Sajeev <narayansajeev1@gmail.com>
2024-05-26 12:02:10 -0400

    Add chart of adulterants by food

--- main.py
+++ main.py
@@ -228,6 +228,68 @@ def food_by_adult():
         pie_cht(df2, 'Distribution of Food by %s' % title, fname, col1, curr_col)
 
 
+def adult_by_food():
+    # Read data
+    df = get_data(food_by_adult)
+
+    # Loop through the rows
+    for _, row in df.iterrows():
+        # Create a new DataFrame
+        df2 = pd.DataFrame(row).reset_index()
+
+        # Set the column names
+        df2.columns = ['x', 'y']
+
+        # Retrieve the food
+        food = df2['y'][0]
+
+        # Remove the first row
+        df2 = df2.iloc[1:]
+
+        # Combine values that are a small portion of the total
+        df2 = create_other_category(df2, 'x', 'y')
+
+        # Remove underscores
+        title = ' '.join(food.split('_'))
+
+        # Remove comma
+        title = ''.join(title.split(','))
+
+        # Capitalize the current row
+        title = ' '.join([_.capitalize() for _ in title.split()])
+
+        # # Split the title into words
+        words = title.split()
+
+        # Loop through each word
+        for word in words:
+            # If that word repeats
+            if words.count(word) > 1:
+                # Find the index of the word
+                i = words.index(word)
+                # Remove the repeated words
+                new_words = words[:i + 1]
+                # Update the new words
+                words = list(new_words)
+
+        # Pluralize the last word
+        last = Pluralizer().pluralize(words[-1])
+        # Replace the last word
+        words[-1] = ''.join(last)
+
+        # Update the title
+        title = ' '.join(words)
+
+        # Retrieve row name
+        row_name = '_'.join([_.lower() for _ in title.split()])
+
+        # Set file name
+        fname = 'adult_in_%s' % row_name
+
+        # Create a pie chart
+        pie_cht(df2, 'Distribution of Adulterant Types in %s' % title, fname, 'x', 'y')
+
+
 loc_by_pct()
 food_by_pct()
 adult_by_pct()
@@ -237,3 +299,4 @@ prov_by_food_pct()
 prov_by_food_count()
 prov_by_recs()
 food_by_adult()
+adult_by_food()
